c0e18c5b6acd5758998d81d79926080295df4b6e
mexdevel <mexdevel@gmail.com>
2019-03-15 02:47:43 -0600

    second commit

--- script.js
+++ script.js
@@ -14,4 +14,20 @@ function divide (a,b){
     return a/b;
 }
 
+let n1 = document.getElementById("n1");
+let n2 = document.getElementById("n2");
+let n3 = document.getElementById("n3");
+let n4 = document.getElementById("n4");
+let n5 = document.getElementById("n5");
+let n6 = document.getElementById("n6");
+let n7 = document.getElementById("n7");
+let n8 = document.getElementById("n8");
+let n9 = document.getElementById("n9");
+let n0 = document.getElementById("n0");
+const bAdd = document.getElementById('#bAdd');
+const bSub = document.getElementById('#bSub');
+const bDiv = document.getElementById('#bDiv');
+const bMul = document.getElementById('#bMul');
+const bRes = document.getElementById('#bRes');
+
 
